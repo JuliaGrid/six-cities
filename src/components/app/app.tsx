74cddca8b5a87d6abda309f9674c610f6a39caf0
JuliaGrid <julia.grid2000@gmail.com>
2024-04-22 17:13:03 +0300

    Routes

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,7 +1,38 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Favorites } from '../../pages/favorites/favorites';
+import { Login } from '../../pages/login/login';
 import { Main } from '../../pages/main/main';
+import { NotFound } from '../../pages/notFound/notFound';
+import { Property } from '../../pages/property/property';
+import { AppRoutes } from './constants';
 
 function App(): JSX.Element {
-  return <Main />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoutes.Main}
+          element={<Main/>}
+        />
+        <Route
+          path={AppRoutes.Login}
+          element={<Login />}
+        />
+        <Route
+          path={AppRoutes.Favorites}
+          element={<Favorites />}
+        />
+        <Route
+          path={AppRoutes.Room}
+          element={<Property />}
+        />
+        <Route
+          path='*'
+          element={<NotFound />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
